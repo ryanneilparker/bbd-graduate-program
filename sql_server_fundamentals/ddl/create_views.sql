5cc55a0a886147bce2c756e537c365db0b85f8f3
Ryan Parker <78847179+ryanneilparker@users.noreply.github.com>
2023-02-23 09:44:08 +0200

    Create create_views.sql

---
+++ sql_server_fundamentals/ddl/create_views.sql
@@ -0,0 +1,24 @@
+create_views.sql
+
+-- Create User Account View
+CREATE VIEW UserAccountInvestments AS
+SELECT [User-Accounts].userAccountID, Users.userName, Accounts.accountType, Instruments.instrumentType, Investments.investmentValue
+FROM [User-Accounts]
+	JOIN Investments ON [User-Accounts].userAccountID = Investments.userAccountID
+	JOIN Instruments ON Investments.instrumentID = Instruments.instrumentID
+	JOIN [Provider-Accounts] ON [User-Accounts].providerAccountID = [Provider-Accounts].providerAccountID
+	JOIN Accounts ON [Provider-Accounts].accountID = Accounts.accountID
+	JOIN Providers ON [Provider-Accounts].providerID = Providers.providerID
+	JOIN Users ON [User-Accounts].userID = Users.userID;
+GO
+
+-- Create Provider Summary View
+CREATE VIEW ProviderAccountSummary AS
+SELECT Providers.providerName, Accounts.accountType, COUNT(DISTINCT [User-Accounts].userID) AS NumUsers, SUM(Investments.investmentValue) AS TotalInvestmentValue
+FROM Providers
+	JOIN [Provider-Accounts] ON Providers.providerID = [Provider-Accounts].providerID
+	JOIN Accounts ON [Provider-Accounts].accountID = Accounts.accountID
+	JOIN [User-Accounts] ON [Provider-Accounts].providerAccountID = [User-Accounts].providerAccountID
+	LEFT JOIN Investments ON [User-Accounts].userAccountID = Investments.userAccountID
+GROUP BY Providers.providerName, Accounts.accountType;
+GO
